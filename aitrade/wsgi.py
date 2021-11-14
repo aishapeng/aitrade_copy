1d5081675b0240acf2aff20b8fe87701844b5202
aishapeng <aishaaishapeng@gmail.com>
2021-11-14 22:09:44 +0800

    first commit

--- aitrade/wsgi.py
+++ aitrade/wsgi.py
@@ -5,5 +5,5 @@ from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aitrade.settings")
 application = get_wsgi_application()
-application = WhiteNoise(application, root='/home/ubuntu/aitrade/static')
+application = WhiteNoise(application, root='/home/ubuntu/aitrade_copy/static')
 # application = WhiteNoise(application, root='/Users/aishapeng/Dev/aitrade/aitrade/static')
